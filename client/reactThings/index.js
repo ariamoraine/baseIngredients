3c3f867231cd6dc178f98dd0b95d5d0c59fa5647
Aria Moraine <ariamoraine@gmail.com>
2017-06-02 16:06:20 -0400

    adding in webpack sass building

--- client/reactThings/index.js
+++ client/reactThings/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import '../../public/index.scss'
 import store from './store'
 
 ReactDOM.render(
