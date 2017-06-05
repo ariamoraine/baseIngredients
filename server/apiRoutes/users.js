8c4541097f490ed139a9a60a0418c582135a9166
Aria Moraine <ariamoraine@gmail.com>
2017-06-05 17:39:32 -0400

    adding login route

--- server/apiRoutes/users.js
+++ server/apiRoutes/users.js
@@ -1,11 +1,28 @@
 const userRouter = require('express').Router()
+const User = require('../database/models/user')
 
 userRouter.get('/', (req, res, next) => {
   res.send('User Router GET to /')
 })
 
-userRouter.post('/', (req, res, next) => {
-  console.log('User Router Post to /')
+userRouter.post('/login', (req, res, next) => {
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  })
+  .then(user => {
+    if (!user) res.status(401).send('User not found')
+    else if (!user.hasMatchingPassword(req.body.password)) {
+      res.status(401).send('Incorrect Password')
+    } else {
+      req.login(user, err => {
+        if (err) next(err)
+        else res.json(user)
+      })
+    }
+  })
+  .catch(next)
 })
 
 userRouter.put('/:userId', (req, res, next) => {
