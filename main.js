d071d8bec0b131d4b85e5bad04699b9a98c8cfc3
Aria Moraine <ariamoraine@gmail.com>
2017-06-05 16:19:17 -0400

    adding in user password salting and hashing

--- main.js
+++ main.js
@@ -6,6 +6,12 @@ const path = require('path');
 
 const apiRoutes = require('./server/apiRoutes')
 const db = require('./server/database')
+//sessions and storing sessions in Sequelize
+const passport = require('passport')
+const session = require('express-session')
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+const dbStore = new SequelizeStore({ db: db})
+
 
 //logging middleware
 app.use(volleyball)
@@ -17,9 +23,33 @@ app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+//session middleware
+dbStore.sync()
+app.use(session({
+  secret: require('./.secrets').sessionSecret,
+  store: dbStore,
+  resave: false,
+  saveUninitialized: false
+}))
+
+//passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user)
+  })
+})
+
 //routing for all api calls
 app.use('/api', apiRoutes)
 
+//routing for everything else
 app.get('*', (req, res, next) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
@@ -31,8 +61,9 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500).send(err.message || 'Internal server error!')
 })
 
+//sync the DB and then start the server lisening
 db.sync({force: true})
-  .then( () => {
+  .then(() => {
     app.listen(3030, () => console.log("Server is listening on port 3030"))
   })
   .catch(console.error);
